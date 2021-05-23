6266331559753f76c2ab335a8344d33388be05b8
tsakib360 <71313906+tsakib360@users.noreply.github.com>
2021-05-23 12:57:08 +0600

    OTP added

---
+++ otp.php
@@ -0,0 +1,22 @@
+<?php include('header.php') ; ?>
+<main>
+        <div class="container">
+
+            <div class="row UMainContain" style="margin:10px 0;">
+                <div class="col-sm-6 col-lg-push-3" style="padding: 10px;">
+                    <form>
+                        <label for="">ওটিপি</label>
+                        <input type="text" class="form-control"><br />
+
+                        <button type="submit" class="btn btn-desh">কনফার্ম করুন</button>
+                    </form>
+
+                </div>
+                <div class="col-sm-3 col-lg-pull-6 ULeftContain">
+
+                </div>
+            </div>
+
+        </div>
+    </main>
+<?php include('footer.php') ; ?>
